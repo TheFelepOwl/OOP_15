04b1a141b37431b0697994b0aae06ae40ea91ec7
TheFelepOwl <denisgavr04@gmail.com>
2023-03-16 22:42:51 +0100

    Create Form3.cs

---
+++ Form3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace OOP_15
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "Kirito"&& textBox2.Text=="Asuna07")
+            {
+                
+                Form1 newForm = new Form1();
+                
+
+                newForm.Show();
+
+                
+
+            }
+
+        }
+    }
+}
